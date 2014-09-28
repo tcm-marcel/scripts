916ec31f2179a9fc8ae1c9d1e295dfdb466d058f
Marcel Kost <mkost1@gmx.de>
2014-09-28 12:59:04 +0200

    updated files

--- VCDdump/VCDdump.py
+++ VCDdump/VCDdump.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-# Python 3
-# @author Marcel Kost <marcel.kost@student.kit.edu>
-
 """
 Dumps the GPIO Ports from a raspberry pi to a VCD-file
 (c) Marcel Kost 2014
@@ -32,7 +29,7 @@ class VCDdump:
 	def add_var(self, name, port, pud):
 		GPIO.setup(port, GPIO.IN, pull_up_down=pud) # GPIO.PUD_UP / GPIO.PUD_DOWN
 		identifier = identifiers.pop(0)
-		vars[port] = (name, identifier)
+		self.vars[port] = (name, identifier, 0)
 	
 	def write_header(self):
 		self.file.write("$date " + time.strftime("%c") + " $end\n")
@@ -40,9 +37,9 @@ class VCDdump:
 		self.file.write("$timescale 1ps $end\n")
 		
 		self.file.write("$scope module RaspberryPi $end\n")
-		self.file.write("$scope module " + module + " $end\n")
+		self.file.write("$scope module " + self.module + " $end\n")
 		
-		for port, (name, identifier) in self.vars.iteritems():
+		for port, (name, identifier, last) in self.vars.items():
 			self.file.write("$var reg 1 " + identifier + " " + name + " $end\n")
 		
 		self.file.write("$upscope $end\n")
@@ -51,38 +48,44 @@ class VCDdump:
 		self.file.write("#0\n")
 		self.file.write("$dumpvars\n")
 	
-	def start():
+	def start(self):
 		self.write_header()
 		
 		self.time_start = time.clock()
 		self.time_last = self.time_start
 		
-		for port, (name, identifier) in self.vars.iteritems():
-			GPIO.add_event_detect(port, GPIO.RISING, callback=port_rising)
-			GPIO.add_event_detect(port, GPIO.FALLING, callback=port_falling)
-	
-	def stop():
-		for port, (name, identifier) in self.vars.iteritems():
+		for port, (name, identifier, last) in self.vars.items():
 			GPIO.remove_event_detect(port)
+			GPIO.add_event_detect(port, GPIO.BOTH, callback=self.port_change)
+			
+			value = GPIO.input(port)
+			self.vars[port] = (name, identifier, GPIO.input(port))
+			self.format_value(identifier, value)
 	
-	def check_port(self, port):
-		if GPIO.input(port) == GPIO.HIGH:
-			return 1
+	def stop(self):
+		for port, (name, identifier, last) in self.vars.items():
+			GPIO.remove_event_detect(port)
+		self.file.close()
+		GPIO.cleanup()
+	
+	def port_change(self, port):
+		time_current = time.clock()
+		if time_current > self.time_last:
+			self.time_last = time_current
+			self.format_time(time_current)
+			
+		name, identifier, last = self.vars[port]
+		value = GPIO.input(port)
+		if value == last:
+			return
+		self.vars[port] = (name, identifier, value)
+		self.format_value(identifier, value)
+	
+	def format_time(self, time_stamp):
+		self.file.write("#" + str(int((time_stamp - self.time_start)*1000)) + "\n")
+	
+	def format_value(self, identifier, value):
+		if value:
+			self.file.write("0" + identifier + "\n")
 		else:
-			return 0
-	
-	def port_rising(port):
-		time = time.clock()
-		if time > self.time_last:
-			self.time_last = time
-			self.file.write("#" + time + "\n")
-		name, identifier = self.vars[port]
-		self.file.write("1" + identifier + "\n")
-	
-	def port_falling(port):
-		time = time.clock()
-		if time > self.time_last:
-			self.time_last = time
-			self.file.write("#" + time + "\n")
-		name, identifier = self.vars[port]
-		self.file.write("0" + identifier + "\n")
+			self.file.write("1" + identifier + "\n")
--- VCDdump/example.py
+++ VCDdump/example.py
@@ -1,35 +1,42 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-# Python 3
-# @author Marcel Kost <marcel.kost@student.kit.edu>
-
 """
 example for VCDdump
 (c) Marcel Kost 2014
 """
 
-import VCDdump
+from VCDdump import VCDdump
+import RPi.GPIO as GPIO
 
 vcd = VCDdump('example.vcd', 'BCM')
 vcd.add_var('GPIO00/02', 3, GPIO.PUD_UP)
-#vcd.add_var('GPIO01/03', 5, GPIO.PUD_UP)
-#vcd.add_var('GPIO04', 7, GPIO.PUD_UP)
-#vcd.add_var('GPIO17', 11, GPIO.PUD_UP)
-#vcd.add_var('GPIO21/27', 13, GPIO.PUD_UP)
-#vcd.add_var('GPIO22', 15, GPIO.PUD_UP)
-#vcd.add_var('GPIO10', 19, GPIO.PUD_UP)
-#vcd.add_var('GPIO09', 21, GPIO.PUD_UP)
-#vcd.add_var('GPIO11', 23, GPIO.PUD_UP)
-#vcd.add_var('GPIO14', 8, GPIO.PUD_UP)
-#vcd.add_var('GPIO15', 10, GPIO.PUD_UP)
-#vcd.add_var('GPIO18', 12, GPIO.PUD_UP)
-#vcd.add_var('GPIO23', 16, GPIO.PUD_UP)
-#vcd.add_var('GPIO24', 18, GPIO.PUD_UP)
-#vcd.add_var('GPIO25', 22, GPIO.PUD_UP)
-#vcd.add_var('GPIO08', 24, GPIO.PUD_UP)
-#vcd.add_var('GPIO07', 26, GPIO.PUD_UP)
+vcd.add_var('GPIO01/03', 5, GPIO.PUD_UP)
+vcd.add_var('GPIO04', 7, GPIO.PUD_UP)
+vcd.add_var('GPIO17', 11, GPIO.PUD_UP)
+vcd.add_var('GPIO21/27', 13, GPIO.PUD_UP)
+vcd.add_var('GPIO22', 15, GPIO.PUD_UP)
+vcd.add_var('GPIO10', 19, GPIO.PUD_UP)
+vcd.add_var('GPIO09', 21, GPIO.PUD_UP)
+vcd.add_var('GPIO11', 23, GPIO.PUD_UP)
+vcd.add_var('GPIO14', 8, GPIO.PUD_UP)
+vcd.add_var('GPIO15', 10, GPIO.PUD_UP)
+vcd.add_var('GPIO18', 12, GPIO.PUD_UP)
+vcd.add_var('GPIO23', 16, GPIO.PUD_UP)
+vcd.add_var('GPIO24', 18, GPIO.PUD_UP)
+vcd.add_var('GPIO25', 22, GPIO.PUD_UP)
+vcd.add_var('GPIO08', 24, GPIO.PUD_UP)
+vcd.add_var('GPIO07', 26, GPIO.PUD_UP)
 
+print('preparing...')
 vcd.start()
+print('starting...')
 
-while
+try:
+	while 1:
+		pass
+except KeyboardInterrupt:
+	vcd.stop()
+	del(vcd)
+	
+	exit()
