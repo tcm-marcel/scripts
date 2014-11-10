b7b509900cb6ad0e56ed39622e60f9487c9a4c0f
Pfeil <der.andreas.pfeil@gmail.com>
2014-11-10 16:05:33 +0100

    implemented printing of multiple files, added farb1 as color printer in comment

--- ATIS print/atis_print.sh
+++ ATIS print/atis_print.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-# so far only for SINGLE DOCUMENTS
-# TODO: add feature to print several documents and whole directories
-
+### Configuration ############################################################
 # specify ATIS account (s_xxx)
 user="s_xxx"
-# select ATIS print to use (sw1/sw2/sw3)
+# select ATIS printer to use (sw1/sw2/sw3 (monochrome) or farb1 (color))
 printer="sw3"
+##############################################################################
 
-file="$1"
+### Known bugs ###############################################################
+# - Files with spaces or other special chars will not be printed.
+##############################################################################
 
-scp "$file" "$user@i08fs1.ira.uka.de:~"
-ssh -l $user i08fs1.ira.uka.de lpr -r -P pool-$printer \"$file\"
+
+### Search for valid files and store them in $files ###
+# This supports regular expressions like ./* and multiple files as parameter
+# but not directories like ./
+# IMPORTANT: currently, files after a non-file parameter will be ignored.
+files=
+while (test $# -gt 0) && (test -f "$1")
+        do
+        files="$files $1"	#files separated with space
+        shift
+done
+if test -z "$files"; then
+        echo "No file given"
+        exit 1
+fi
+
+
+echo "Copy files to ATIS account: "
+scp $files "$user@i08fs1.ira.uka.de:~"
+
+
+echo "Print files and delete copies on ATIS account: "
+ssh -l $user i08fs1.ira.uka.de lpr -r -P pool-$printer $files
