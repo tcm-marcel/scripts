08b48a9031ad3674021be33d0714db6aeec5a58c
Pfeil <der.andreas.pfeil@gmail.com>
2014-12-04 01:59:08 +0100

    fixed usage of range and compact in combination

--- ATIS print/README.md
+++ ATIS print/README.md
@@ -6,26 +6,27 @@ This script is for students at the KIT[1], who have access to an ATIS[2] account
 
 The order of parameters is completely irrelevant.
 
-    .../ATIS_print [file(s)]* [-u user] [-p printer] [-n number-of-copies] [-r from to] [-c num]
+    .../ATIS_print [file(s)] [-u user] [-p printer] [-n number-of-copies] [-r from to] [-c num]
 
-- **files:** Regular expressions and single files are allowed.
+- **files:** Regular expressions and files are allowed.
 - **-u user:** Specify a user for printing. This will dominate the default, but not override it permanently. Like a one-time-ticket. Useful for quickly printing things for others.
 - **-p printer:** Use given printer. Must be sw1, sw2, sw3 (monochrome) or farb1 (color).
 - **-n number-of-copies:** Specify how many times the files should be printed. Use integers only.
-- **-r from to:** Specify a page **r**ange using integers as `from` and `to`. Only pages within this range will be printed if this is used. Otherwise the whole file will be printed. Note that this currently points at the page range, not the document range.
-- **-c num:** Compact / N-up print. This will print num pages on one page. `num` can be 2,4,6 or 9. This has to be used carefully when `-r` is also used.
+- **-r from to:** Specify a page **r**ange using integers as `from` and `to`. Only pages within this range will be printed if this is used. Currently, only one range is supported. May not work with every format. If used with `-c`, only PDFs will be limited to this range.
+- **-c num:** Compact / N-up print. This will print `num` pages on one page. `num` can be 2,4,6 or 9.
 
 ## Dependencies
 
 It depends on basic applications which are default on most unix-like operating systems. Tested on Ubuntu Linux and OSX Mavericks. 
 
+    # required dependencies
     bash, scp, ssh
+    # optional dependencies
+    gs  #to use -r together with -c, see usage of -r
 
 ## Contribution wishlist, ideas and bugs
 
 - BUG: Files with same names in different directories in one atis\_print call cause problems.
-- Bug: -c influences -r in a bad way (see usage). This should be fixed at least for PDFs.
-- Allow to print more pages on one paper (like `-#p 4`, see `man lp`). Note that this has impact on the -r option, see [CUPS docs][pageranges]
 - For text/code print jobs, allow to adjust the text size (like `--txt-size 8`)
 - Allow to print one/two-sided (like `-nd` or `--no-duplex`, also long- or short-sided?)
 - Create Install/setup scripts for easier usage (install i.e. to `~/bin/`)
@@ -38,4 +39,3 @@ It depends on basic applications which are default on most unix-like operating s
 
 [3] Those accounts have usernames like `s_user`. It's given to students of informatics and business informatics.
 
-[pageranges]: http://cups.org/documentation.php/doc-1.7/options.html?Q=lpr#PAGERANGES
--- ATIS print/atis_print.sh
+++ ATIS print/atis_print.sh
@@ -5,6 +5,8 @@
 user="s_xxx"
 # select ATIS printer to use (sw1/sw2/sw3 (monochrome) or farb1 (color))
 printer="sw3"
+# path to temporary directory for this script
+dirTMP=/tmp/atis
 ##############################################################################
 
 ### Known bugs ###############################################################
@@ -14,18 +16,30 @@ printer="sw3"
 
 
 ### Parse arguments ##########################################################
-# Example input: ./atis_print file1 file2 directory/* -u s_muster -p sw2
 fpaths=		#file paths (array)
 fnames=		#file names (array)
 fcount=0	#amount of files to be printed (integer)
 skipNum=0	#amount of invalid parameters  (integer)
 printNum=	#amount of copies for all given files (string)
 pages=		#range of pages to print (for each file!) (string)
+lowEnd=		#lower end of page range (see line above this one)
+upperEnd=	#upper end of page range (see line above this one)
+PDF=false	#return value of isPDF() $1 (boolean)
 
 stage() {
 	fpaths[$fcount]="$1"
 	fnames[$fcount]="\"$(basename "$1")\""
 	let "fcount += 1"
+}
+
+isPDF() {
+        local info=`file -b "$1"`
+        info=${info:0:3}
+        if (test "$info" = "PDF") then
+                PDF=true
+        else
+                PDF=false
+        fi
 }
 
 while (test $# -gt 0); do
@@ -49,6 +63,8 @@ while (test $# -gt 0); do
 		shift
 	elif (test "$1" = "-r") && (test "$2" != "") && (test "$3" != "") then
 		shift
+		lowEnd=$1
+		upperEnd=$2
 		pages="-o page-ranges=$1-$2"
 		shift
 		shift
@@ -76,7 +92,7 @@ fi
 ###############################################################################
 
 
-### Review output && DEBUG options ############################################
+### Review output #############################################################
 if (test $skipNum -ne 0;) then
 	echo "WARNING: $skipNum parameters are invalid and ignored."
 fi
@@ -88,17 +104,46 @@ if (test "$pages" != "";) then
         echo "Print pages ${pages:15} of every file."
 fi
 echo "Files: ${fpaths[@]}"
-#echo "fnames: ${fnames[@]}"	#DEBUG
-#echo "scp ${fpaths[@]} \"$user@i08fs1.ira.uka.de:~\""	#DEBUG
-#echo "ssh -l $user i08fs1.ira.uka.de lpr -r -P pool-$printer $printNum $compact $pages ${fnames[@]}"
-#exit 0	#DEBUG
 echo "To abort, press CTRL+C when you are asked for the password."
 ###############################################################################
 
 
 ### Printing process ##########################################################
+#first, if -r AND -c is used:
+#create PDFs with the range given in -r
+#and use those to print, so -r works with -c properly.
+#Only do this if really necessary (-r in combination with -c is used)
+#because this fix needs `pdfjam` as dependency and takes some time.
+if (test "$pages" != "") && (test "$compact" != "") then
+        mkdir $dirTMP
+        for i in $(seq 0 ${#fpaths[@]})
+                do
+                isPDF "${fpaths[$i]}" #return value is $PDF
+                if ($PDF) then
+			#filter pdf pages and save to /tmp/atis/
+			gs -sDEVICE=pdfwrite -q -dNOPAUSE -dBATCH -dSAFER \
+				-dFirstPage=$lowEnd \
+				-dLastPage=$upperEnd \
+				-sOutputFile=$dirTMP/$(basename "${fpaths[$i]}") \
+				"${fpaths[$i]}"
+
+			# change filepath to filtered versions
+                        fpaths[$i]=$dirTMP/$(basename "${fpaths[$i]}")
+                fi
+        done
+fi
+
+### DEBUG OPTIONS ##################
+#echo "------- DEBUG INFOS --------"
+#echo "FILENAMES: ${fnames[@]}"
+#echo "FILEPATHS: ${fpaths[@]}"
+#exit 0
+####################################
+
+# transfer and print
 echo "Copy files to ATIS account: "
 scp "${fpaths[@]}" "$user@i08fs1.ira.uka.de:~"
 echo "Print files and delete copies on ATIS account: "
 ssh -l $user i08fs1.ira.uka.de lpr -r -P pool-$printer $printNum $compact $pages "${fnames[@]}"
+rm -rf $dirTMP
 ###############################################################################
